5c58e36ce26f6b180677a52e7a232ee0e739dfc7
彭星樺 <ktpss97094.cs09@m365.nycu.edu.tw>
2023-01-28 08:18:56 +0000

    aaa

--- sa.py
+++ sa.py
@@ -2,39 +2,40 @@ import random
 
 from iottalkpy.dan import NoData
 
-### The registeration api url, you can use IP or Domain.
-api_url = 'http://localhost:9992'  # default
+# The registeration api url, you can use IP or Domain.
+# api_url = 'http://localhost:9992'  # default
 # api_url = 'http://localhost/csm'  # with URL prefix
 # api_url = 'http://localhost:9992/csm'  # with URL prefix + port
+api_url = 'https://iottalk2.tw/csm'
 
-### [OPTIONAL] If not given or None, server will auto-generate.
+# [OPTIONAL] If not given or None, server will auto-generate.
 # device_name = 'Dummy_Test'
 
-### [OPTIONAL] If not given or None, DAN will register using a random UUID.
-### Or you can use following code to use MAC address for device_addr.
+# [OPTIONAL] If not given or None, DAN will register using a random UUID.
+# Or you can use following code to use MAC address for device_addr.
 # from uuid import getnode
 # device_addr = "{:012X}".format(getnode())
 # device_addr = "..."
 
-### [OPTIONAL] If the device_addr is set as a fixed value, user can enable
-### this option and make the DA register/deregister without rebinding on GUI
+# [OPTIONAL] If the device_addr is set as a fixed value, user can enable
+# this option and make the DA register/deregister without rebinding on GUI
 # persistent_binding = True
 
-### [OPTIONAL] If not given or None, this device will be used by anyone.
+# [OPTIONAL] If not given or None, this device will be used by anyone.
 # username = 'myname'
 
-### The Device Model in IoTtalk, please check IoTtalk document.
+# The Device Model in IoTtalk, please check IoTtalk document.
 device_model = 'Dummy_Device'
 
-### The input/output device features, please check IoTtalk document.
+# The input/output device features, please check IoTtalk document.
 idf_list = ['DummySensor-I']
 odf_list = ['DummyControl-O']
 
-### Set the push interval, default = 1 (sec)
-### Or you can set to 0, and control in your feature input function.
-push_interval = 10  # global interval
+# Set the push interval (IDF多久發送資料一次), default = 1 (sec)
+# Or you can set to 0, and control in your feature input function.
+push_interval = 10  # global interval (指定所有的IDF的interval)
 interval = {
-    'Dummy_Sensor': 3,  # assign feature interval
+    'DummySensor-I': 3,  # assign feature interval (指定個別的IDF的interval)
 }
 
 
@@ -42,7 +43,7 @@ def on_register(dan):
     print('register successfully')
 
 
-def DummySensor_I():
+def DummySensor_I():  # function名稱需為DF的名稱
     return random.randint(0, 100)
 
     # Or you want to return nothing.
@@ -51,5 +52,5 @@ def DummySensor_I():
     # return NoData
 
 
-def DummyControl_O(data: list):
+def DummyControl_O(data: list):  # function名稱需為DF的名稱
     print(str(data[0]))
